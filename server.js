616bd1e780b5a00c7481004d15f1726ff5f65c86
Gaurav Kohli <gauravkohli174@gmail.com>
2020-04-22 15:33:08 -0400

    added mongoose in server script

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://heroku_3jg27jhq:6o6ar6nvffe9lj7hj3baoedh21@ds135444.mlab.com:35444/heroku_3jg27jhq', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -10,7 +15,12 @@ app.use(function (req, res, next) {
     next();
 });
 
-require('./controllers/quiz.controller.server')(app)
-require('./controllers/question.controller.server')(app)
+require('./controllers/quiz.controller.server')(app);
+require('./controllers/question.controller.server')(app);
+
+const bodyParser = require( 'body-parser' );
+app.use ( bodyParser.urlencoded ({ extended : false }));
+app.use ( bodyParser.json ());
+require('./controllers/quiz-attempts.controller.server')(app)
 
 app.listen(process.env.PORT || 3000)
