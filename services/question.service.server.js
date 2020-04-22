71d16e330ebf8990e68a448d7c22ccb327aa9535
Gaurav Kohli <gauravkohli174@gmail.com>
2020-04-22 15:31:53 -0400

    added methods in services

--- services/question.service.server.js
+++ services/question.service.server.js
@@ -1,8 +1,7 @@
-let questions = require('./questions')
 
-findQuestionsForQuiz = (quizId) =>
-    questions.filter(question => question.quizId === quizId)
+const questionsDao = require( '../daos/questions.dao.server' )
+const findAllQuestions = () => questionsDao .findAllQuestions()
+const findQuestionById = (qid) => questionsDao .findQuestionById(qid)
+const findQuestionsForQuiz = (qid) => questionsDao .findQuestionsForQuiz(qid)
 
-module.exports = {
-    findQuestionsForQuiz
-}
+module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
--- services/quiz.service.server.js
+++ services/quiz.service.server.js
@@ -1,8 +1,6 @@
-let quizzes = require('./quizzes')
-const findAllQuizzes = () => quizzes
-const findQuizById = (quizId) =>
-    quizzes.find(quiz => quiz._id === quizId)
-module.exports = {
-    findAllQuizzes,
-    findQuizById
-}
+
+const quizzesDao = require( '../daos/quizzes.dao.server' )
+const findAllQuizzes = () => quizzesDao .findAllQuizzes()
+const findQuizById = (qid) => quizzesDao .findQuizById(qid)
+
+module.exports = { findAllQuizzes , findQuizById }
