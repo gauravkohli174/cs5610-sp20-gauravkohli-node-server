91308a989669eb7294d99b3444a07a480c4e58ff
Gaurav Kohli <gauravkohli174@gmail.com>
2020-04-22 15:32:14 -0400

    added quiz attempts and quizzes models and schema

---
+++ models/quizzes/quizzes.models.server.js
@@ -0,0 +1,8 @@
+const mongoose = require( 'mongoose' )
+const quizSchema = require( './quizzes.schema.server' )
+const quizModel = mongoose . model (
+    'QuizzesModel' ,
+    quizSchema
+)
+
+module.exports = quizModel
